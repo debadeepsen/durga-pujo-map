82bddb6305bf8f29263fa93c47cb1a8b79f8f01c
Debadeep Sen (MSIK) <debadeep.sen@gmail.com>
2025-09-16 23:48:40 +0530

    tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
     <main>
       <div className='flex flex-1 items-center border-b border-zinc-600/50 z-10'>
         {/* <img src='/dhak.jpg' className='ml-2' style={{ height: 48 }} /> */}
-        <h1 className='text-xl font-semibold h-[48px] flex flex-1 items-center pl-4 text-red-500 dark:text-red-400/90'>
+        <h1 className='text-xl font-semibold h-[48px] flex flex-1 items-center pl-4'>
           Durga Puja Pandals Map
         </h1>
       </div>
