6885a7d831cde80bac03dbf060e1307850e78459
Debadeep Sen (MSIK) <debadeep.sen@gmail.com>
2025-09-22 17:23:38 +0530

    filter code cleanup

--- src/components/PujaList.tsx
+++ src/components/PujaList.tsx
@@ -8,6 +8,7 @@ import GoogleMapLink from './GoogleMapLink'
 import { SearchBox } from './ui/SearchBox'
 import { Details, Summary } from './ui/accordion/components'
 import { getCategorizedData } from '@/utils/utils'
+import { ClassifiedPujas, PandalInfo } from '@/types/types'
 
 type PujaListProps = {
   onSelect?: (lat: number, lng: number, name: string) => void
@@ -17,103 +18,33 @@ const PujaList = ({ onSelect }: PujaListProps) => {
   const [searchQuery, setSearchQuery] = useState('')
   const [expandedSections, setExpandedSections] = useState<string[]>([])
 
-  // Transform the data to match the expected format
-  // const data = useMemo<ClassifiedPujas>(() => {
-  //   const result: ClassifiedPujas = {}
-
-  //   // Initialize all categories from categoryMap
-  //   Object.keys(categoryMap).forEach(category => {
-  //     result[category] = []
-  //   })
-
-  //   // Populate the categories with pandal data
-  //   pandals.forEach(pandal => {
-  //     const { latitude, longitude } = pandal.location
-  //     const { name, category, description } = pandal.details
-
-  //     const feature: PandalFeature = {
-  //       type: 'Feature',
-  //       geometry: {
-  //         type: 'Point',
-  //         coordinates: [longitude, latitude] // Note: GeoJSON uses [lng, lat]
-  //       },
-  //       properties: {
-  //         name,
-  //         description:
-  //           typeof description === 'string' ? description : description.value
-  //       }
-  //     }
-
-  //     if (result[category]) {
-  //       result[category].push(feature)
-  //     } else {
-  //       // If category doesn't exist in categoryMap, add it to 'other' category
-  //       if (!result['south']) result['south'] = []
-  //       result['south'].push(feature)
-  //     }
-  //   })
-
-  //   // Remove empty categories
-  //   Object.keys(result).forEach(key => {
-  //     if (result[key].length === 0) {
-  //       delete result[key]
-  //     }
-  //   })
-
-  //   return result
-  // }, [])
-
-  // // Filter data based on search query
-  // const filteredData = useMemo(() => {
-  //   if (!searchQuery.trim()) return data
-
-  //   const query = searchQuery.toLowerCase()
-  //   const result: ClassifiedPujas = {}
-  //   const sectionsWithMatches = new Set<string>()
-
-  //   Object.entries(data).forEach(([category, features]) => {
-  //     const matchingFeatures = features.filter(feature =>
-  //       feature.properties.name.toLowerCase().includes(query)
-  //     )
-
-  //     if (matchingFeatures.length > 0) {
-  //       result[category] = matchingFeatures
-  //       sectionsWithMatches.add(category)
-  //     }
-  //   })
-
-  //   // Update expanded sections based on search results
-  //   setExpandedSections(prev => {
-  //     const newSections = new Set(prev)
-  //     sectionsWithMatches.forEach(section => newSections.add(section))
-  //     return newSections
-  //   })
-
-  //   return result
-  // }, [data, searchQuery])
-
-  // // Use filtered data or all data if no search query
-  // const displayData = searchQuery.trim() ? filteredData : data
-
-  // const handleSectionToggle = (category: string) => {
-  //   setExpandedSections(prev => {
-  //     const newSections = new Set(prev)
-  //     if (newSections.has(category)) {
-  //       newSections.delete(category)
-  //     } else {
-  //       newSections.add(category)
-  //     }
-  //     return newSections
-  //   })
-  // }
-
   const data = useMemo(() => getCategorizedData(pandals), [pandals])
+  const filteredData = useMemo(() => {
+    const filtered: ClassifiedPujas = {
+      aristocratic: [],
+      north: [],
+      salt_lake: [],
+      south: []
+    }
 
-  // if (Object.keys(data).length === 0) {
-  //   return <Typography>No puja pandals found.</Typography>
-  // }
+    const categoryMatch: string[] = []
+    pandals.forEach(pandal => {
+      const { name, category } = pandal.details
+      if (name.toLowerCase().includes(searchQuery.toLowerCase())) {
+        filtered[category as keyof ClassifiedPujas].push(pandal)
+        categoryMatch.push(category)
+      }
+    })
+
+    setExpandedSections([...new Set(categoryMatch)])
+
+    return filtered
+  }, [pandals, searchQuery])
+
+  const displayData = searchQuery.trim() ? filteredData : data
 
   const handleToggle = (isExpanded: boolean, category: string) => {
+    console.log({ isExpanded, category })
     setExpandedSections(prev => {
       const newSections = [...prev]
       if (!isExpanded) {
@@ -121,14 +52,15 @@ const PujaList = ({ onSelect }: PujaListProps) => {
       } else {
         newSections.push(category)
       }
-      return newSections
+      console.log(newSections)
+      return [...new Set(newSections)]
     })
   }
 
   return (
     <div>
       <SearchBox onSearch={setSearchQuery} className='mb-2' />
-      {Object.entries(data).map(([category, pandals]) => (
+      {Object.entries(displayData).map(([category, pandalList]) => (
         <Accordion
           key={category}
           expanded={expandedSections.includes(category)}
@@ -145,11 +77,11 @@ const PujaList = ({ onSelect }: PujaListProps) => {
           </Summary>
           <Details>
             <List>
-              {pandals.map((pandal, idx) => {
+              {(pandalList as PandalInfo[]).map(pandal => {
                 const { latitude, longitude } = pandal.location
                 const { name } = pandal.details
                 return (
-                  <div key={idx}>
+                  <div key={pandal.id}>
                     <button
                       className='flex items-center gap-2 mb-2 w-full text-left'
                       onClick={() => onSelect?.(latitude, longitude, name)}
