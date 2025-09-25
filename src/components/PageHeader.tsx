c462f468a5b632167a77ac1f85f9ed6b909d3404
unknown <vikash@IQINM106.corp.inquest.com>
2025-09-25 18:13:49 +0530

    Added an about page

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,9 +1,32 @@
+'use client';
+
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
 export const PageHeader = () => {
+  const pathname = usePathname();
+  const isAboutPage = pathname === '/about';
+  const linkText = isAboutPage ? 'Home' : 'About';
+  const linkHref = isAboutPage ? '/' : '/about';
+
   return (
     <div className='flex flex-1 items-center border-b border-zinc-600/50 z-10'>
       <h1 className='text-xl font-semibold h-[48px] flex flex-1 items-center pl-12 relative -top-[1px]'>
         Durga Puja Pandals Map
       </h1>
+
+
+      {/* About link on top-right */}
+      <nav className="pr-12">
+        <Link
+          href={linkHref}
+          className="text-sm font-medium text-blue-600 hover:text-blue-800"
+        >
+          {linkText}
+        </Link>
+      </nav>
     </div>
+
+
   )
 }
